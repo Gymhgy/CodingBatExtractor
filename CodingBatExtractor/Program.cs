bdba016c4add51a7a31725b4b7863aa105dd306a
Yifan Wang <cheetahyf@gmail.com>
2021-06-29 14:39:52 -0700

    Add project files.

---
+++ CodingBatExtractor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace CodingBatExtractor {
+    class Program {
+        static void Main(string[] args) {
+            var config = ConfigurationManager.AppSettings;
+            string username = config.Get("username");
+            string password = config.Get("password");
+            string filepath = config.Get("filepath");
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
