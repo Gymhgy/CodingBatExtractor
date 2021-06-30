4a884158ac2cf7a375b8b4b00a7bca3d3ace9970
Yifan Wang <cheetahyf@gmail.com>
2021-06-29 21:40:13 -0700

    Login to codingbat

--- CodingBatExtractor/Program.cs
+++ CodingBatExtractor/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Configuration;
 using System.Collections.Specialized;
+using System.Net;
+using System.Net.Http;
+using System.Collections.Generic;
 
 namespace CodingBatExtractor {
     class Program {
@@ -11,7 +14,20 @@ namespace CodingBatExtractor {
             string filepath = config.Get("filepath");
             Console.WriteLine("Hello World!");
 
+            //Login
+            var cookieContainer = new CookieContainer();
+            HttpClientHandler handler = new HttpClientHandler() { CookieContainer = cookieContainer };
+            HttpClient client = new HttpClient(handler) { BaseAddress = new Uri("codingbat.com/") };
 
+            var content = new FormUrlEncodedContent(new Dictionary<string, string>
+            {
+                { "uname", username },
+                { "password", password },
+                { "dologin", "login" },
+                { "fromurl", "/java" }
+            });
+
+            var login = client.PostAsync("/login", content).Result;
         }
     }
 }
