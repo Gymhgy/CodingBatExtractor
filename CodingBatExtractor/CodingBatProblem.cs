6a127d15569557bf5e63ad0385d95d467be7c017
Yifan Wang <cheetahyf@gmail.com>
2021-06-30 15:50:38 -0700

    Parse and Process, also got rid of config for username + password and filepath

---
+++ CodingBatExtractor/CodingBatProblem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingBatExtractor {
+    class CodingBatProblem {
+        public CodingBatProblem(string problemName, string problemGroup, string link, string description, string implementation) {
+            ProblemName = problemName;
+            ProblemGroup = problemGroup;
+            Link = link;
+            Description = description;
+            Implementation = implementation;
+        }
+
+        public string ProblemName { get; }
+        public string ProblemGroup { get; }
+        public string Link { get; }
+        public string Description { get; }
+        public string Implementation { get; }
+
+
+        public string Format() {
+            return 
+$@"package {ProblemGroup.Replace('-', '_')};
+
+import java.util.*;
+import java.util.stream.*;
+
+public class {ProblemName[0].ToString().ToUpper() + ProblemName.Substring(1)} {{
+
+    /**
+     * {Description}
+     * Problem Source: {Link}
+     */
+    {Implementation}
+
+}}";
+        }
+    }
+}
--- CodingBatExtractor/Program.cs
+++ CodingBatExtractor/Program.cs
@@ -1,33 +1,68 @@
 ﻿using System;
-using System.Configuration;
-using System.Collections.Specialized;
 using System.Net;
+using System.Linq;
+using HtmlAgilityPack;
 using System.Net.Http;
 using System.Collections.Generic;
+using System.IO;
+using System.Text;
 
 namespace CodingBatExtractor {
     class Program {
         static void Main(string[] args) {
-            var config = ConfigurationManager.AppSettings;
-            string username = config.Get("username");
-            string password = config.Get("password");
-            string filepath = config.Get("filepath");
-            Console.WriteLine("Hello World!");
+            string username = args[0];
+            string password = args[1];
+            string filepath = args[2];
 
+            string codingbat = "https://codingbat.com";
             //Login
             var cookieContainer = new CookieContainer();
             HttpClientHandler handler = new HttpClientHandler() { CookieContainer = cookieContainer };
-            HttpClient client = new HttpClient(handler) { BaseAddress = new Uri("codingbat.com/") };
-
+            HttpClient client = new HttpClient(handler) { BaseAddress = new Uri(codingbat) };
             var content = new FormUrlEncodedContent(new Dictionary<string, string>
             {
                 { "uname", username },
-                { "password", password },
-                { "dologin", "login" },
+                { "pw", password },
+                { "dologin", "log in" },
                 { "fromurl", "/java" }
             });
 
             var login = client.PostAsync("/login", content).Result;
+            foreach (var cookie in cookieContainer.GetCookies(new Uri(codingbat))) {
+                Console.WriteLine(cookie.ToString()); // test=testValue
+            }
+            //Parse
+            var doneHtml = client.GetStringAsync("/done").Result;
+            var doc = new HtmlDocument();
+            doc.OptionEmptyCollection = true;
+            doc.LoadHtml(doneHtml);
+            var parsed = doc.DocumentNode.SelectNodes("//a[contains(@href,'/prob/')]").Select(prob => {
+                var innertext = prob.InnerText.Split();
+                var link = prob.Attributes["href"].Value;
+                var probHtml = client.GetStringAsync(link).Result;
+                var probDoc = new HtmlDocument();
+                probDoc.LoadHtml(probHtml);
+                return new CodingBatProblem(
+                    link: codingbat + link,
+                    problemGroup: innertext[0],
+                    problemName: innertext[1],
+                    description: probDoc.DocumentNode.SelectSingleNode("//p[@class='max2']").InnerText,
+                    implementation: probDoc.DocumentNode.SelectSingleNode("//div[@id='ace_div']").InnerText);
+            }).ToList();
+
+            //Process
+            parsed.ForEach(prob => {
+                var formatted = prob.Format();
+                var path = Path.Combine(filepath, prob.ProblemGroup);
+                //Create directory doesn't do anything if directory already exists
+                //So don't need to check
+                Directory.CreateDirectory(path);
+
+                using (FileStream fs = File.Create(Path.Combine(path, prob.ProblemName))) {
+                    byte[] bytes = new UTF8Encoding().GetBytes(formatted);
+                    fs.Write(bytes, 0, bytes.Length);
+                }
+            });
         }
     }
 }
